ecb8991bdd397c0762ec5ce1322724e3096f4fdf
corina <ciorpaccorina@gmail.com>
2023-01-22 00:08:51 +0100

    2intento de proyecto

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 
-function start(){
-    var video = document.getElementById(video);
-    video.play();
-}
+// function start(){
+//     var video = document.getElementById(video);
+//     video.play();
+// }
+str.start = function() {
+    document.getElementById("video").play();
+};
+
+str.pausar = function() {
+    document.getElementById("video").pause();
+};
